ed7fa820290c208ec6796c2ead7da22fb99fc1a3
Ozy Wu-Li <ozy.wu.li@gmail.com>
2015-01-04 02:11:24 -0800

    Added more content

---
+++ build/js/all.js
@@ -0,0 +1,136 @@
+$(function() {
+
+  var $navOpen = $('.js-nav-open');
+  var $navClose = $('.js-nav-close');
+  var $header = $('.header');
+  var $offsetLeft = $('.offset-left');
+
+  $navOpen.on('click', function(e) {
+    e.preventDefault();
+    $(this).fadeOut(100);
+    $navClose.fadeIn(200);
+    $header.animate({
+      left: 0
+    }, 200)
+    $offsetLeft.animate({
+      paddingLeft: 200
+    }, 200)
+  })
+
+  $navClose.on('click', function(e) {
+    e.preventDefault();
+    $navOpen.fadeIn();
+    $header.animate({
+      left: -160
+    }, 200)
+    $offsetLeft.animate({
+      paddingLeft: 20
+    }, 200)
+  })
+
+  var $windowWidth = $(window).width();
+
+  $(window).resize(function() {
+    console.log($header.css( 'left' ));
+    console.log($windowWidth);
+    if ( $windowWidth > 800 && $header.css( 'left' ) === '-160px' ) {
+      $header.removeAttr('style');
+      $offsetLeft.removeAttr('style');
+      $navOpen.removeAttr('style');
+      $navClose.removeAttr('style');
+    }
+  })
+
+});
+
+
+
+$(function() {
+
+
+var $container = $('.js-portfolio-items').imagesLoaded( function() {
+  $container.isotope({
+    // options
+    itemSelector: '.all'
+  });
+
+});
+
+
+
+$('.js-portfolio-option').click(function() {
+
+  var selector = $(this).attr('data-filter');
+
+  $('.js-portfolio-option').removeClass('active-filter');
+  $(this).addClass('active-filter');
+
+  $container.isotope({
+    filter: selector
+  });
+
+  return false;
+});
+
+
+});
+
+
+
+
+
+$(function() {
+
+  var $scrolltop = $('.scrolltop');
+
+
+  $(window).scroll(function() {
+
+    if ( $(this).scrollTop() > $('.js-portfolio-options').offset().top ) {
+      $scrolltop.fadeIn();
+    } else {
+      $scrolltop.fadeOut();
+    }
+
+  })
+
+
+
+
+  $scrolltop.on('click', function(e) {
+    e.preventDefault();
+    $('body').animate({
+      "scrollTop": 0
+    }, 400);
+  });
+
+});
+
+
+
+// $(document).ready(function() {
+
+
+// $('body').fadeIn(1000);
+
+
+
+// $('.nav__link').click(function(event) {
+
+// event.preventDefault();
+
+// newLocation = this.href;
+
+// $('body').fadeOut(1000, newpage);
+
+// });
+
+
+
+// function newpage() {
+
+// window.location = newLocation;
+
+// }
+
+// });
